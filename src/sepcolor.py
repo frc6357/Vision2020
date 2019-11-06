b56a80055f32bd7cd50438c75ade3f0fb0abf0fc
vivek-ramadhar <vivek.ramadhar@gmail.com>
2019-11-05 18:20:34 -0600

    uses load image function and makes all color black except if the color lies within the specified hue range using loaded image

--- src/sepcolor.py
+++ src/sepcolor.py
@@ -2,16 +2,25 @@ import cv2
 import numpy as np
 
 im = cv2.imread("../CargoSideStraightDark36in.jpg")
-im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-print(im)
-# upper bound +5%: HSV (171.15, 105, 91.875)
-# lower bound -5%: HSV (154.85, 95, 83.125)
-image_lower_bound = np.array([154.85, 95.000, 83.125])
-image_upper_bound = np.array([171.15, 105, 91.875])
-imageFilter = cv2.inRange(im, image_lower_bound, image_upper_bound)
-im = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
-imageFiltered = cv2.bitwise_and(im, im, mask=imageFilter)
-window_name = "name?"
+# reads image in previous directory and stores BGR values as an array in variable im
+im2 = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+# looks at BGR values in array im and then converts values by mathematically transforming them into HSV values
+
+# Color/shade?/hue? of green to be isolated RGB values: [0, 224, 163]
+# supposed HSV values: [82, 255, 224]
+image_lower_bound = np.uint8([[[154.85, 212.8, 0]]])
+# -5% [0, 212.8, 154.85]
+image_lower_bound = cv2.cvtColor(image_lower_bound, cv2.COLOR_RGB2HSV)
+# HSV converted values of lower bound
+# HSV converted value: [38, 255, 212]
+image_upper_bound = np.uint8([[[171.15, 235.2, 0]]])
+# +5% [0, 235.2, 171.15]
+image_upper_bound = cv2.cvtColor(image_upper_bound, cv2.COLOR_RGB2HSV)
+# HSV converted values of lower bound
+# HSV converted value: [38, 255, 235]
+filteredImage = cv2.inRange(im2, image_lower_bound, image_upper_bound)
+im3 = cv2.cvtColor(im2, cv2.COLOR_HSV2BGR)
+imageFiltered = cv2.bitwise_and(im3, im3, mask = filteredImage)
 cv2.imshow("Filtered Image", imageFiltered)
-cv2.imshow("Image" , im)
+cv2.imshow("Image", im)
 cv2.waitKey(0)
