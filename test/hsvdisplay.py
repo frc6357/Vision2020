e87b4680a6379e4644ac9a87617adc6ed2a431ac
vivek-ramadhar <vivek.ramadhar@gmail.com>
2019-11-19 18:05:47 -0600

    makes a matplot graph of a given color in hsv color three values and displays the color in rgb

--- test/hsvdisplay.py
+++
@@ -1,8 +0,0 @@
-from matplotlib.colors import hsv_to_rgb
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2
-im = cv2.imread("../green-purple.png")
-targetHSV = [120, 100, 97]
-im1 = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
-print(im1)
