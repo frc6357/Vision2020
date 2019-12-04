6067b6027239418928b7376b0a67b9c2f38c1388
vivek-ramadhar <vivek.ramadhar@gmail.com>
2019-12-03 18:15:30 -0600

    a script that defines a function that inputs hue, saturation, and brightness value along with a threshold percent to create an upper and lower bound and then outputs hue, saturation, and brightness values that have been thresholded as well as compatible with OpenCV

--- src/create_bound.py
+++ src/create_bound.py
@@ -1,5 +1,19 @@
+
+"""
+This script takes in a hue(h) in degrees out of 360, saturation(s) as percent, and brightness(v) as a percent and then a
+percentage (in decimal form: 100% = 1.0) to increase or decrease those values to create upper and lower bounds
+
+"""
+
 import numpy as np
 
+"""
+This method outputs an array that OpenCV will read and output accurately with the diffrence being 
+OpenCV divides h value by two scales the value out of 255 
+ex: 
+
+HSV = [120, 100, 97] but OpenCV will need [60, 255, 247.35]
+"""
 
 def bound_percent_cv2(h, s, v, percent):
     h = (h * percent)/2
@@ -8,6 +22,10 @@ def bound_percent_cv2(h, s, v, percent):
     lower_bound = np.array([h, s, v])
     return lower_bound
 
+"""
+This method takes in the h, s, v and threshold percent values and outputs hue in degrees out of 360(s), saturation(s) percent, and brightness(v) percent
+"""
+
 def bound_percent(h, s, v, percent):
     h = h * percent
     v = v * percent
@@ -15,6 +33,7 @@ def bound_percent(h, s, v, percent):
     lower_bound = np.array([h, s, v])
     return lower_bound
 
-lower_bound = bound_percent(120, 100, 97, 0.95)
-print(lower_bound)
+if __name__ == '__main__':
+    lower_bound = bound_percent_cv2(120, 100, 97, 1)
+    print(lower_bound)
 
