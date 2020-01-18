ef4468e0d083dadccac0c6a45e69981d24deb0af
vivek-ramadhar <vivek.ramadhar@gmail.com>
2020-01-18 15:44:08 -0600

    this script reads an image and filters the image until it is just edges. It then uses the edges to draw lines in the image using the Hough Transform and finds where those lines intersect This is completed through the OpenCV library and various other fuctions we have made to complete this. We are working on recognizing regular triangles in the images.

--- src/sepcolor.py
+++ src/sepcolor.py
@@ -1,9 +1,16 @@
 # This script filters colors by turning the pixels black that fall within a tolerance of HSV values
 import cv2
+import time
+import itertools
 import numpy as np
-from src.create_bound import*
+from src.create_bound import *
+from src.systemeq import *
+from src.reg_triangle_detect import *
+from src.mb_to_xy import *
 
-im = cv2.imread("../2020SampleVisionImages/WPILib_Robot_Vision_Images/RedLoading-108in.jpg")
+ts_start = time.time()
+
+im = cv2.imread("../2020SampleVisionImages/WPILib_Robot_Vision_Images/BlueGoal-108in-Center.jpg")
 
 """
 cv2.cvtColor() takes in a variable storing a read image
@@ -27,14 +34,14 @@ Ex: input HSV = [120, 100, 97] and threshold image by +5%
 image_upper_bound = bound_percent_cv2(120, 100, 97, 1.05)
 """
 
-image_lower_bound = bound_percent_cv2(153, 100, 81, 0.85)
-image_upper_bound = bound_percent_cv2(153, 100, 81, 1.15)
+image_lower_bound = bound_percent_cv2(153, 100, 81, 0.7)
+image_upper_bound = bound_percent_cv2(153, 100, 81, 1.3)
 
 """
 cv2.inRange() takes in a variable storing a read image, lower bound, and upper bound
 It then checks each each pixel to see if it lies within the lower and upper bound
 It will return a true(1) or false(0) and if the pixel returns 0 then this method
-turns that pixel black, if the pixel returns 1 then this method does noting to it
+turns that pixel black, if the pixel returns 1 then this method does nothing to it
 """
 imageFilter = cv2.inRange(im, image_lower_bound, image_upper_bound)
 imageFiltered = cv2.bitwise_and(im, im, mask=imageFilter)
@@ -46,11 +53,118 @@ back to BGR in order to display interpretable images
 """
 
 imageFiltered = cv2.cvtColor(imageFiltered, cv2.COLOR_HSV2BGR)
+
+#Converts BGR to Grayscale image in preparation for thresholding by making a bimodal image
+
+grayscale_im = cv2.cvtColor(imageFiltered, cv2.COLOR_BGR2GRAY)
+
+
+ret2, th2 = cv2.threshold(grayscale_im, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+"""
+laplacian = cv2.Laplacian(grayscale_im, cv2.CV_64F)
+
+cv2.imshow("Laplacian Edge Detect", laplacian)
+
+sobelx = cv2.Sobel(grayscale_im,cv2.CV_64F,1,0,ksize=3)
+
+abs_sobel64f = np.absolute(sobelx)
+sobelx_8u = np.uint8(abs_sobel64f)
+
+cv2.imshow("Sobel X Edge Detect", sobelx_8u)
+
+sobely = cv2.Sobel(grayscale_im,cv2.CV_64F,0,1,ksize=3)
+
+abs_sobel64f = np.absolute(sobely)
+sobely_8u = np.uint8(abs_sobel64f)
+
+
+cv2.imshow("Sobel Y Edge Detect", sobely_8u)
+"""
+edges = cv2.Canny(grayscale_im, 100, 200)
+
+cv2.imshow("Canny Edge Detection", edges)
+
+
+
+lines = cv2.HoughLines(edges, 1, np.pi/120, 60)
+#need a check to make sure we find lines
+
+points = []
+slope_offset = []
+print(len(lines))
+for line in lines:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    m1 = (y2-y1)/(x2-x1)
+    b1 = y1 - m1*x1
+    slope_offset.append([m1, b1])
+    cv2.line(im, (x1, y1), (x2, y2), (0, 255, 255), 2)
+
+points = [solve_syseq(pair) for pair in itertools.combinations(slope_offset, 2)]
+
+points = [point for point in points if point is not None]
+print(points)
+
+for point in points:
+    cv2.circle(im, point, 5, (60, 255, 255))
+
+
+triangles = [detect_tri(a) for a in itertools.combinations(slope_offset, 3)]
+triangles = [a for a in triangles if a is not None]
+
+print(len(triangles))
+
+tri_points = [mb_xy(a) for a in triangles]
+tri_points = [a for a in tri_points if a[0] is not None]
+
+print(tri_points)
+
+#triangles_list_pairs = mb_xy(triangles)
+
+#print(triangles_list_pairs)
+
+
+"""
+
+tri_m_np = np.array([a[0] for a in tri_m_b])
+
+tri_b_np = np.array([a[1] for a in tri_m_b])
+
+"""
+
+
+
+
+
+
+
+
+
+cv2.imshow("Grayscale Otsu Thresholded Image", th2)
+# cv2.imshow() takes in a string that is windows name and then a variable that stores the image
+
+cv2.imshow("Filtered Image", imageFiltered)
+cv2.imshow("Grayscale Image", grayscale_im)
+# window name will be Filtered Image
+
 im = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
 
-# cv2.imshow() takes in a string that is windows name and then a variable that stores the image
-cv2.imshow("Filtered Image", imageFiltered)
-# window name will be Filtered Image
+
 cv2.imshow("Original Image", im)
 # window name will be Original Image
+ts_end = time.time()
+
+
+runtime = ts_end-ts_start
+print(runtime, "total time")
 cv2.waitKey(0)
