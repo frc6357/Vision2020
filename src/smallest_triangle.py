039147987c410af3c7d8db67605cb04de8dcf33c
vivek-ramadhar <vivek.ramadhar@gmail.com>
2020-01-20 17:33:37 -0600

    this script finds the distance between each of the 3 points between two given triangles and finds the triangle with the smallest distance

---
+++ src/smallest_triangle.py
@@ -0,0 +1,28 @@
+import math
+
+def min_tri(input):
+    first, second, third = input[0]
+    fourth, fifth, sixth = input[1]
+
+    t1_d1 = math.sqrt((second[0]-first[0])**2 + (second[1] - first[1])**2)
+
+    t1_d2 = math.sqrt((second[0] - third[0]) ** 2 + (second[1] - third[1]) ** 2)
+
+    t1_d3 = math.sqrt((third[0] - first[0]) ** 2 + (third[1] - first[1]) ** 2)
+
+
+    t2_d1 = math.sqrt((fifth[0] - fourth[0])**2 + (fifth[1] - fourth[1])**2)
+
+    t2_d2 = math.sqrt((fifth[0] - sixth[0]) ** 2 + (fifth[1] - sixth[1]) ** 2)
+
+    t2_d3 = math.sqrt((sixth[0] - fourth[0]) ** 2 + (sixth[1] - fourth[1]) ** 2)
+
+    t1_max_length = max(t1_d1, t1_d2, t1_d3)
+    t2_max_length = max(t2_d1, t2_d2, t2_d3)
+
+    if t1_max_length < t2_max_length:
+        return first, second, third
+    else:
+        return fourth, fifth, sixth
+
+
