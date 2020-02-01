5d74c1a3ad0ab109fdd254ae01f3ab6560e6aae0
vivek-ramadhar <vivek.ramadhar@gmail.com>
2020-02-01 17:04:33 -0600

    no commit message for now

--- src/camera_feed.py
+++ src/camera_feed.py
@@ -17,7 +17,7 @@ cap = cv2.VideoCapture(1)
 ts_mid = time.time()
 
 print(ts_mid-ts_start, "seconds to start")
-while(True):
+while True:
     # Capture frame-by-frame
 
     ret, frame = cap.read()
@@ -39,10 +39,190 @@ while(True):
 
     cv2.imshow("Edges", edges)
 
+    lines = []
+    """
+    for i in range(60, -1, -5):
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, i)
+        num_lines = len(lines)
+        if num_lines in range(8, 21):
+            new_list = [a[0, 1] for a in lines]
+            valid_lines = valid_line_test(lines, new_list)
+            break
+    """
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    for i in range(60, 6, -5):
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, i)
+        if type(lines) != str:
+            lines = check_valid(lines, 0.1)
+            lines = check_valid(lines, 0.1)
+            num_lines = len(lines)
+            if num_lines == 6:
+                break
+    if type(lines) == str:
+        continue
 
-# When everything done, release the capture
+    # valid_line_test(lines, )
+
+    print("threshold of: ", i)
+    print("number of lines: ", num_lines)
+
+    # need a check to make sure we find lines
+
+    points = []
+    slope_offset = []
+    for line in lines:
+        rho, theta = line[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+        if x2 == x1:
+            continue
+        m1 = (y2 - y1) / (x2 - x1)
+        b1 = y1 - m1 * x1
+        slope_offset.append([m1, b1])
+        cv2.line(im, (x1, y1), (x2, y2), (0, 255, 255), 2)
+    """
+    a for loop that takes the output from hough lines (rho, theta) and transforms them 
+    into slopes and intercepts and appends them to an array  
+    """
+
+    im1 = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
+    im2 = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
+
+
+
+    points = [solve_syseq(pair) for pair in itertools.combinations(slope_offset, 2)]
+    """
+    takes slopes and intercepts from the slope_offset array and uses list comprehension
+    to loop through each element and find the intersections between the lines
+    """
+
+    points = [point for point in points if point is not None]
+    #removes nones from the list
+
+    for point in points:
+        cv2.circle(im, point, 5, (60, 255, 255))
+    #draws each intersection found from previous list comprehension
+
+    triangles = [detect_tri(a) for a in itertools.combinations(slope_offset, 3)]
+    """
+    using the slopes and intercepts from slope offset detect_tri() finds all the lines that intersect 
+    and make regular triangles, using list comprehension to loop through each element 
+    """
+    triangles = [a for a in triangles if a is not None]
+
+
+    tri_points = [mb_xy(a) for a in triangles]
+    tri_points = [a for a in tri_points if a[0] is not None]
+    tri_points = [a for a in tri_points if a[1] is not None]
+    tri_points = [a for a in tri_points if a[2] is not None]
+
+    tri_in_img = [in_img(a, h, w) for a in tri_points]
+    tri_in_img = [a for a in tri_in_img if a[0] is not None]
+    tri_in_img = [a for a in tri_in_img if a[1] is not None]
+    tri_in_img = [a for a in tri_in_img if a[2] is not None]
+
+    longest_edge = [longest_side(a) for a in tri_in_img]
+
+    equ_tri = [draw_eq_tri(a) for a in longest_edge]
+
+    largest_triangle = [a for a in equ_tri]
+
+    while len(largest_triangle) > 1:
+        largest_triangle = [max_tri(a) for a in itertools.combinations(largest_triangle, 2)]
+        largest_triangle = [a for a in largest_triangle if a is not None]
+
+        largest_triangle = remov_dupl(largest_triangle)
+
+    largest_triangle = [a for t in largest_triangle for a in t]
+
+    original_im = cv2.imread("../2020SampleVisionImages/WPILib_Robot_Vision_Images/BlueGoal-108in-Center.jpg")
+    for a in largest_triangle:
+        cv2.circle(original_im, a, 5, (0, 255, 255))
+
+    centroid_point = centroid(largest_triangle)
+
+    cv2.circle(original_im, centroid_point, 5, (0, 255, 255))
+
+    """
+    if all(tri_in_img) == False:
+        print("triangle is outside of image")
+        cv2.waitKey(0)
+        exit()
+    """
+
+    """
+    largest_triangle = [a for a in tri_in_img]
+
+    while len(largest_triangle) > 1:
+        largest_triangle = [max_tri(a) for a in itertools.combinations(largest_triangle, 2)]
+        largest_triangle = [a for a in largest_triangle if a is not None]
+
+        largest_triangle = remov_dupl(largest_triangle)
+
+    largest_triangle = [a for t in largest_triangle for a in t]
+    triangle = []
+
+    smallest_triangle = [a for a in tri_in_img]
+
+    while len(smallest_triangle) > 1:
+        smallest_triangle = [min_tri(a) for a in itertools.combinations(smallest_triangle, 2)]
+        smallest_triangle = [a for a in smallest_triangle if a is not None]
+
+        smallest_triangle = remov_dupl(smallest_triangle)
+
+    smallest_triangle = [a for t in smallest_triangle for a in t]
+    """
+    """
+    largest_triangle = [max_tri(a) for a in itertools.combinations(largest_triangle, 2)]
+    largest_triangle = [a for a in largest_triangle if a is not None]
+    largest_triangle = remov_dupl(largest_triangle)
+
+    print(largest_triangle)
+
+    for a in largest_triangle:
+        cv2.circle(im1, a, 5, (60, 255, 255))
+    cv2.imshow("Largest Triangle", im1)
+
+    for a in smallest_triangle:
+        cv2.circle(im2, a, 5, (60, 255, 255))
+    cv2.imshow("Smallest Triangle", im2)
+
+    """
+    """
+    tri_m_np = np.array([a[0] for a in tri_m_b])
+
+    tri_b_np = np.array([a[1] for a in tri_m_b])
+    """
+
+
+
+    # cv2.imshow() takes in a string that is windows name and then a variable that stores the image
+
+
+
+    # window name will be Filtered Image
+
+    im = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
+
+
+    ts_end = time.time()
+    runtime = ts_end - ts_start
+    print(runtime, "total time")
+
+    # cv2.imshow("Equilateral Triangles", original_im)
+    cv2.imshow("Original Image", im1)
+    # window name will be Original Image
+
+    cv2.waitKey(0)
+
+
+
 cap.release()
 cv2.destroyAllWindows()
--- src/reg_triangle_detect.py
+++ src/reg_triangle_detect.py
@@ -6,7 +6,7 @@ def detect_tri(input):
     m1 = first[0]
     m2 = second[0]
     m3 = third[0]
-    accept_angles = [0, 1, 2, 57, 58, 59]
+    accept_angles = [0, 1, 2, 3, 55, 56, 57, 58, 59]
     theta1 = int(math.atan(m1) * 180/math.pi)
     theta_01 = theta1 if theta1 <= 180 else 360-theta1
     theta_01 = abs(theta_01)
@@ -23,20 +23,26 @@ def detect_tri(input):
     theta3 = theta3-theta_offset
 
     if theta1-theta2 in accept_angles:
+        print(theta1-theta2)
         return None
     if theta1-theta3 in accept_angles:
+        print(theta1-theta3)
         return None
     if theta2-theta3 in accept_angles:
+        print(theta2-theta3)
         return None
 
     if (theta1 + theta2) % 60 not in accept_angles:
+        print((theta1 + theta2) % 60)
         return None
 
     if (theta1 + theta3) % 60 not in accept_angles:
+        print((theta1 + theta3) % 60)
         return None
 
 
     if (theta2 + theta3) % 60 not in accept_angles:
+        print((theta2 + theta3) % 60)
         return None
 
 
